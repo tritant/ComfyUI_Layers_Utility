dad16cf725877ab9700b490691cb1dad295771d6
tritant <tritant@hotmail.com>
2025-09-05 16:50:00 +0200

    code cleanup

--- js/removebg.js
+++ js/removebg.js
@@ -2,7 +2,7 @@ export class RemoveBgManager {
     constructor(node, maskManager) {
         this.node = node;
         this.maskManager = maskManager;
-        this.button = null; // Référence au bouton pour le feedback visuel
+        this.button = null;
     }
 
 async performRemoveBg() {
@@ -14,12 +14,12 @@ async performRemoveBg() {
     const sourceFilename = previewInfo?.filename;
 
     if (!sourceFilename) {
-        alert("Erreur : Impossible de trouver le nom du fichier source.");
+        alert("Erreur : Unable to find source file name.");
         return;
     }
 
     if (this.button) {
-        this.button.textContent = "Traitement...";
+        this.button.textContent = "processing...";
         this.button.disabled = true;
     }
 
@@ -30,24 +30,18 @@ async performRemoveBg() {
             body: JSON.stringify({ filename: sourceFilename, layer_index_str: String(layerIndex)}),
         });
 
-        if (!response.ok) throw new Error(`Erreur du serveur : ${await response.text()}`);
+        if (!response.ok) throw new Error(`Server error : ${await response.text()}`);
 
         const newMasks = await response.json();
         const previewMaskDetails = newMasks.preview_mask_details;
         const renderMaskDetails = newMasks.render_mask_details;
-
-        // --- On distribue les masques à la bonne destination ---
-        
-        // 1. Pour le BACKEND : on stocke les détails du masque de RENDU (noir/blanc)
         const layerProps = this.node.layer_properties[layerName];
         layerProps.internal_mask_filename = renderMaskDetails.name;
         layerProps.internal_mask_details = renderMaskDetails;
         layerProps.internal_preview_mask_details = previewMaskDetails;
 		layerProps.mask_last_update = Date.now();
-        // On ne touche PAS au toggle, il reste à sa valeur manuelle.
         this.node.updatePropertiesJSON();
 
-        // 2. Pour la PREVIEW LIVE : on charge le masque de PREVIEW (blanc/noir)
         const previewMaskUrl = new URL("/view", window.location.origin);
         previewMaskUrl.searchParams.set("filename", previewMaskDetails.name);
         previewMaskUrl.searchParams.set("type", previewMaskDetails.type);
@@ -69,8 +63,7 @@ async performRemoveBg() {
         this.node.redrawPreviewCanvas();
 
     } catch (error) {
-        console.error("[LayerSystem] Erreur lors du Remove BG:", error);
-        alert(`Une erreur est survenue lors du Remove BG: ${error.message}`);
+        console.error("[LayerSystem] Error while removing BG:", error);
     } finally {
         if (this.button) {
             this.button.textContent = "Remove BG";
